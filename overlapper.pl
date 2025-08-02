1963ee05aaa695ce54957f16013086104309daee
AI-IS-POWER <EricZzc12345@outlook.com>
2025-08-02 20:14:48 +0800

    Update overlapper.pl

--- overlapper.pl
+++ overlapper.pl
@@ -1,1 +1,21 @@
-($a,$b)=@ARGV; for($i=length$a;$i>=0;$i--){ last if substr($a,-$i) eq substr($b,0,$i) } say $a.substr($b,$i)'
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+# grab inputs
+my ($a, $b) = @ARGV;
+unless (defined $a && defined $b) {
+    die "Usage: $0 word1 word2\n";
+}
+
+# find maximal overlap
+my $overlap = 0;
+for (my $i = length($a); $i >= 0; $i--) {
+    if (substr($a, length($a) - $i, $i) eq substr($b, 0, $i)) {
+        $overlap = $i;
+        last;
+    }
+}
+
+# print result
+print $a . substr($b, $overlap), "\n";
